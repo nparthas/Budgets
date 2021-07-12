bd2a8c08550b3df96f4bc7732b997b79fc20badf
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-07-12 19:09:47 -0400

    deleting graph

--- app/desktop/src/components/ExpensesCard.js
+++ app/desktop/src/components/ExpensesCard.js
@@ -18,13 +18,6 @@ const ExpensesCard = (props) => {
     },
   };
 
-  const data = [
-    { quarter: 1, earnings: 13000 },
-    { quarter: 2, earnings: 16500 },
-    { quarter: 3, earnings: 14250 },
-    { quarter: 4, earnings: 19000 },
-  ];
-
   const card_type = props.type.toLowerCase();
 
   return (
@@ -36,27 +29,7 @@ const ExpensesCard = (props) => {
     >
       <div className="card-inner">
         <Link to={`/${props.type}`}>
-          <div className="card-face">
-            <VictoryPie
-              style={{
-                data: {
-                  fillOpacity: 0.9,
-                  stroke: "#333",
-                  strokeWidth: 2,
-                },
-                labels: {
-                  fontSize: 20,
-                  fill: "#333",
-                },
-              }}
-              data={data}
-              x="quarter"
-              y="earnings"
-              colorScale={["purple", "silver", "cyan", "lime"]}
-              labelRadius={({ innerRadius }) => innerRadius + 40}
-              innerRadius={0}
-            />
-          </div>
+          <div className="card-face">{/* expenses */}</div>
         </Link>
       </div>
     </motion.div>
