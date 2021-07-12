04fcbe0b5b6f124eb7dfcaff7534836da21a4a3f
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-07-12 00:04:27 -0400

    organizing

--- app/desktop/src/components/ExpensesCard.js
+++ app/desktop/src/components/ExpensesCard.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import ".././css/statistics-card.css";
+import ".././css/expenses-card.css";
 import { Link } from "react-router-dom";
 import { VictoryPie } from "victory";
 
