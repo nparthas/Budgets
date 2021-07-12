d858dba411c7078e09c3c2231be5d90f19c7418a
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-07-11 21:14:37 -0400

    changes

--- app/desktop/src/components/CardHome.js
+++ app/desktop/src/components/CardHome.js
@@ -1,32 +1,62 @@
 import { motion } from "framer-motion";
 import ".././css/card.css";
 import { Link } from "react-router-dom";
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryPie } from "victory";
 
 const CardHome = (props) => {
   const expandVariants = {
     hidden: {
       opacity: 0,
-      scale: 0.5,
+      scale: 0.8,
     },
     visible: {
       opacity: 1,
       scale: 1,
       transition: {
-        duration: 0.3,
+        duration: 0.5,
       },
     },
   };
 
+  const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+  ];
+
+  const card_type = props.type.toLowerCase();
+
   return (
     <motion.div
-      className="card"
+      className={`card ${card_type}`}
       variants={expandVariants}
       initial="hidden"
       animate="visible"
     >
       <div className="card-inner">
         <Link to={`/${props.type}`}>
-          <div className="card-face card-face--front">{props.type}</div>
+          <div className="card-face card-face--front">
+            <VictoryPie
+              style={{
+                data: {
+                  fillOpacity: 0.9,
+                  stroke: "#333",
+                  strokeWidth: 2,
+                },
+                labels: {
+                  fontSize: 20,
+                  fill: "#333",
+                },
+              }}
+              data={data}
+              x="quarter"
+              y="earnings"
+              colorScale={["tomato", "orange", "gold", "cyan", "navy"]}
+              labelRadius={({ innerRadius }) => innerRadius + 40}
+              innerRadius={0}
+            />
+          </div>
         </Link>
       </div>
     </motion.div>
