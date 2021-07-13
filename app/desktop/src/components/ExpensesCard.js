55d9023f21e81f5a3a6124cb76c9a66c79acae54
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-07-12 20:08:17 -0400

    table is in

--- app/desktop/src/components/ExpensesCard.js
+++ app/desktop/src/components/ExpensesCard.js
@@ -1,7 +1,15 @@
 import { motion } from "framer-motion";
 import ".././css/expenses-card.css";
 import { Link } from "react-router-dom";
-import { VictoryPie } from "victory";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
 
 const ExpensesCard = (props) => {
   const expandVariants = {
@@ -20,6 +28,18 @@ const ExpensesCard = (props) => {
 
   const card_type = props.type.toLowerCase();
 
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
+
   return (
     <motion.div
       className={`card ${card_type}`}
@@ -29,7 +49,29 @@ const ExpensesCard = (props) => {
     >
       <div className="card-inner">
         <Link to={`/${props.type}`}>
-          <div className="card-face">{/* expenses */}</div>
+          <div className="card-face">
+            {/* expenses */}
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Dessert (100g serving)</TableCell>
+                    <TableCell align="right">Calories</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow key={row.name}>
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
         </Link>
       </div>
     </motion.div>
