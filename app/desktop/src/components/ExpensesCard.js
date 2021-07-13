73faa29e273f6d673ee2650e090ada31bfcb316d
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-07-12 22:07:39 -0400

    using data from props for pie

--- app/desktop/src/components/ExpensesCard.js
+++ app/desktop/src/components/ExpensesCard.js
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import ".././css/expenses-card.css";
 import { Link } from "react-router-dom";
 import {
+  makeStyles,
   Table,
   TableBody,
   TableCell,
@@ -10,6 +11,13 @@ import {
   TableRow,
   Paper,
 } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  table: {
+    maxWidth: 320,
+    padding: "5px",
+  },
+});
 
 const ExpensesCard = (props) => {
   const expandVariants = {
@@ -27,19 +35,7 @@ const ExpensesCard = (props) => {
   };
 
   const card_type = props.type.toLowerCase();
-
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-  ];
-
+  const classes = useStyles();
   return (
     <motion.div
       className={`card ${card_type}`}
@@ -50,8 +46,8 @@ const ExpensesCard = (props) => {
       <div className="card-inner">
         <Link to={`/${props.type}`}>
           <div className="card-face">
-            {/* expenses */}
-            <TableContainer component={Paper}>
+            <h2 className="card-header">Expenses</h2>
+            <TableContainer className={classes.table} component={Paper}>
               <Table aria-label="simple table">
                 <TableHead>
                   <TableRow>
@@ -60,7 +56,7 @@ const ExpensesCard = (props) => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {rows.map((row) => (
+                  {props.data.map((row) => (
                     <TableRow key={row.name}>
                       <TableCell component="th" scope="row">
                         {row.name}
--- app/desktop/src/components/Home.js
+++ app/desktop/src/components/Home.js
@@ -1,12 +1,27 @@
 import StatisticsCard from "./StatisticsCard";
 import ExpensesCard from "./ExpensesCard";
 
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 15900, 6.0, 24, 1),
+  createData("Ice cream sandwich", 23700, 9.0, 37, 2),
+  createData("Eclair", 26200, 16.0, 24, 3),
+  createData("Cupcake", 30500, 3.7, 67, 4),
+];
+
 const Home = () => {
   return (
     <div>
-      <StatisticsCard type={"Statistics"}></StatisticsCard>
-      <ExpensesCard type={"Expenses"}></ExpensesCard>
-      <StatisticsCard type={"Upcoming"}></StatisticsCard>
+      <StatisticsCard
+        type={"Statistics"}
+        protein={rows.protein}
+        calories={rows.calories}
+      ></StatisticsCard>
+      <ExpensesCard type={"Expenses"} data={rows}></ExpensesCard>
+      {/* <StatisticsCard type={"Upcoming"} data={rows}></StatisticsCard> */}
     </div>
   );
 };
