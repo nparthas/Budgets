f91b2cd19c7998730f7a19f57de3f752d053a127
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-07-27 17:35:22 -0400

    removed css assing navigation

--- app/desktop/src/components/Navbar.js
+++ app/desktop/src/components/Navbar.js
@@ -1,5 +1,8 @@
 import ".././css/navbar.css";
 import { Link, useLocation } from "react-router-dom";
+import { IconButton } from "@material-ui/core";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import HomeIcon from "@material-ui/icons/Home";
 
 const Navbar = () => {
   const cur_path = useLocation().pathname;
@@ -7,16 +10,43 @@ const Navbar = () => {
 
   var new_expense;
   if (title === "Expenses") {
-    new_expense = <Link to="/NewExpense">New</Link>;
+    new_expense = (
+      <Link to="/NewExpense">
+        <IconButton>
+          <AddCircleOutlineIcon />
+        </IconButton>
+      </Link>
+    );
   }
+
+  var new_chart;
+  if (title === "Statistics") {
+    new_chart = (
+      <Link to="/NewChart">
+        <IconButton disableRipple={true} disableFocusRipple={true}>
+          <AddCircleOutlineIcon />
+          <p>New Chart</p>
+        </IconButton>
+      </Link>
+    );
+  }
+
+  var home = (
+    <Link to="/">
+      <IconButton disableFocusRipple={true}>
+        <HomeIcon />
+      </IconButton>
+    </Link>
+  );
 
   return (
     <nav className="navbar">
       <h1>{title}</h1>
       {/* <h1>Hi</h1> */}
+      {new_expense}
+      {new_chart}
       <div className="links">
-        {new_expense}
-        <Link to="/">Home</Link>
+        {home}
         {/* <Link to="/Expenses">Ex</Link>
         <Link to="/Upcoming">Up</Link>
         <Link to="/NewExpense">Cr Ex</Link> */}
