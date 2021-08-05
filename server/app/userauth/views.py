4a97666877dfb05d9ede765b0734a43f9f183a4b
Neil <neilp1998@gmail.com>
2021-08-04 23:56:34 -0400

    modify token refresh view to pull refresh token from cookies -- this is the recommended way to send the refresh token leaving the 'refresh' field blank

--- server/app/userauth/views.py
+++ server/app/userauth/views.py
@@ -1,10 +1,25 @@
 from allauth.account.views import LogoutFunctionalityMixin
-from rest_framework import generics, permissions, response, status
+from django.conf import settings
+from rest_framework import generics, permissions, response, serializers, status
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .models import User
 
 
-# Create your views here.
+class TokenRefreshCookieSerializer(TokenRefreshSerializer):
+    def validate(self, attrs):
+        if 'refresh' not in attrs:
+            attrs['refresh'] = self.context['request'].COOKIES[settings.JWT_AUTH_REFRESH_COOKIE]  # noqa: E501
+        return super(TokenRefreshCookieSerializer, self).validate(attrs)
+
+
+# pylint: disable=maybe-no-member
+TokenRefreshCookieSerializer._declared_fields['refresh'] = serializers.CharField(  # noqa: E501
+    required=False, allow_null=True)
+TokenRefreshView.serializer_class = TokenRefreshCookieSerializer
+
+
 class UserDeleteView(LogoutFunctionalityMixin, generics.GenericAPIView):
 
     permission_classes = [permissions.IsAuthenticated]
